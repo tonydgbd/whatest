8ce5748e643baec12a6a0869658603d0317f1297
DONGOBADA Antony Christian Davy <72560692+tonydgbd@users.noreply.github.com>
2024-11-03 11:03:52 +0000

    Refactor conversation-state.service.ts to use a shared Redis client
    This commit refactors the ConversationStateService in conversation-state.service.ts to use a shared Redis client. Previously, a new Redis client was created for each instance of the service, leading to unnecessary resource consumption. Now, a single Redis client is created and reused across all instances of the service, improving efficiency.

--- src/conversation-state/conversation-state.service.ts
+++ src/conversation-state/conversation-state.service.ts
@@ -1,35 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { createClient } from 'redis';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { createClient, RedisClientType } from 'redis';
+
 @Injectable()
-export class ConversationStateService {
-  redisClient = createClient({
-    url: 'redis://default:ZyCjq3OCl8WwyA907VYeUEnKSqumFrRS@redis-16687.c253.us-central1-1.gce.redns.redis-cloud.com:16687',
-  });
+export class ConversationStateService implements OnModuleInit, OnModuleDestroy {
+  private static redisClient: RedisClientType;
 
   constructor() {
-    this.redisClient.on('error', (err) =>
-      console.log('Redis Client Error', err),
-    );
+    if (!ConversationStateService.redisClient) {
+      ConversationStateService.redisClient = createClient({
+        url: process.env.Redis_URL,
+      });
 
-    (async () => {
-      await this.redisClient.connect();
-    })();
+      ConversationStateService.redisClient.on('error', (err) =>
+        console.log('Redis Client Error', err),
+      );
+    }
   }
+
+  async onModuleInit() {
+    if (!ConversationStateService.redisClient.isOpen) {
+      await ConversationStateService.redisClient.connect();
+    }
+  }
+
+  async onModuleDestroy() {
+    if (ConversationStateService.redisClient.isOpen) {
+      await ConversationStateService.redisClient.quit();
+    }
+  }
+
   async getConversationState(userId: string, Whatsapp_ph_id: string) {
-    const state = await this.redisClient.get(userId + '_' + Whatsapp_ph_id);
+    const state = await ConversationStateService.redisClient.get(userId + '_' + Whatsapp_ph_id);
     return state ? JSON.parse(state) : null;
   }
-  async updateConversationState(
-    userId: string,
-    state: any,
-    Whatsapp_ph_id: string,
-  ) {
-    await this.redisClient.set(
+
+  async updateConversationState(userId: string, state: any, Whatsapp_ph_id: string) {
+    await ConversationStateService.redisClient.set(
       userId + '_' + Whatsapp_ph_id,
       JSON.stringify(state),
       {
-        "EXAT": Date.now() + 24 * 60 * 60 * 1000,
-      }
+        EXAT: Date.now() + 24 * 60 * 60 * 1000,
+      },
     );
   }
 }
